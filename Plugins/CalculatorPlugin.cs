4dc4988efcee12eb4d49dc167e31a1ef8f2018fa
Isak Pao <windperson@gmail.com>
2025-07-12 15:51:31 +0800

    Update documentation

--- Plugins/CalculatorPlugin.cs
+++ Plugins/CalculatorPlugin.cs
@@ -4,9 +4,6 @@ namespace MySemanticKernelApp.Plugins
 {
     public class CalculatorPlugin
     {
-        // The SKFunction attribute and Microsoft.SemanticKernel.Attributes namespace
-        // were removed in Semantic Kernel v1.0+. Plugin methods are now discovered by convention.
-
         [KernelFunction("Add")]
         public double Add(double a, double b) => a + b;
 
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# SKDemo
+# MySemanticKernelApp
+
+This repository contains a sample application that demonstrates how to use the Semantic Kernel SDK with .NET 8.0.
